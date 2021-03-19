f77ea7c79e3dd3e1ebb62b2727bff6281fd08905
Justin Schroeder <justin.schroeder3@gmail.com>
2021-03-19 15:22:29 -0500

    Commit to main

--- phrase-hunter/phrasehunter/game.py
+++ phrase-hunter/phrasehunter/game.py
@@ -1,1 +1,31 @@
-# Create your Game class logic in here.
+from phrase import Phrase
+import random
+
+class Game:
+    def __init__(self):
+        self.missed = 0
+        self.phrases = ['A Dime a Dozen', 'Cut to the Chase', 'Put a Sock In It', 'Fish Out of Water', 'A Chip on Your Shoulder']
+        self.active_phrase = None
+        self.guesses = []
+
+    def start(self):
+        pass
+
+    def get_random_phrase(self):
+        self.active_phrase = random.choice(self.phrases)
+        return
+
+    def welcome(self):
+        pass
+
+    def get_guess(self):
+        pass
+
+    def game_over(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.get_random_phrase()
+    print(game.active_phrase)
--- phrase-hunter/phrasehunter/phrase.py
+++ phrase-hunter/phrasehunter/phrase.py
@@ -1,1 +1,33 @@
-# Create your Phrase class logic here.
+class Phrase():
+    def __init__(self, phrase):
+        self.phrase = phrase
+
+    def display(self, guesses):
+        for letter in self.phrase:
+            if letter in guesses:
+                print(letter, end=' ')
+            elif letter == ' ':
+                print(letter, end=' ')
+            else:
+                print('_', end=' ')
+
+    def check_letter(self, letter):
+        if letter in self.phrase:
+            return True
+        else:
+            return False
+
+    def check_complete(self, guesses):
+        for letter in self.phrase:
+            if letter not in guesses and letter != ' ':
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    phrase = Phrase('hello world')
+    letter = 'o'
+    print(phrase.check_letter(letter))
+    guesses = ['h','e','l','o','w','r','d']
+    print(phrase.check_complete(guesses))
+    phrase.display(guesses)
